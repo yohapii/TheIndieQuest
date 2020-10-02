081233ad4f72333e7ce143663d7d4ee870835341
yohapii <37801852+yohapii@users.noreply.github.com>
2020-10-02 05:48:43 +0200

    Week 3 Day 1 Mission 1 Part 2

--- w03d01m01/w03d01m01/Program.cs
+++ w03d01m01/w03d01m01/Program.cs
@@ -6,49 +6,165 @@ using System.Threading.Tasks;
 
 namespace w03d01m01 {
     class Program {
+        static Random rand = new Random();
+
         static void Main(string[] args) {
-            Pins();
-            Pins(       // Following pin is standing?
-                true,   // Pin 1
-                false,  // Pin 2
-                true,   // Pin 3
-                false,  // Pin 4
-                true,   // Pin 5
-                false,  // Pin 6
-                true,   // Pin 7
-                false,  // Pin 8
-                true,   // Pin 9
-                false   // Pin 10
-                );
+            BowlingGame();
         }
 
-        static void Pins(bool pin1 = true, bool pin2 = true, bool pin3 = true, bool pin4 = true, bool pin5 = true, bool pin6 = true, bool pin7 = true, bool pin8 = true, bool pin9 = true, bool pin10 = true) {
-            var pinsStanding = new List<bool> { pin1, pin2, pin3, pin4, pin5, pin6, pin7, pin8, pin9, pin10 };
+        static void BowlingGame() {
+            int totalFrames = 10;
 
+            int firstThrow = 0;
+            int secondThrow = 0;
+
+            for (int currentFrame = 0; currentFrame < totalFrames; currentFrame++) {
+                var standingPinsList = new List<bool> { true, true, true, true, true, true, true, true, true, true };
+
+                for (int i = 0; i < 3; i++) {
+                    Console.WriteLine($"FRAME {currentFrame + 1}");
+                    if (i == 0) {
+                        DrawFrame();
+                        DrawPins(standingPinsList);
+                    }
+                    else if (i == 1) {
+                        firstThrow = rand.Next(0, 11);
+                        DrawFrame(firstThrow);
+
+                        // Generating the base pattern of standing pins based on firstThrow.
+                        standingPinsList.Clear();
+                        for (int j = 0; j < 10; j++) {
+                            if (firstThrow < j + 1) {
+                                standingPinsList.Add(true);
+                            }
+                            else {
+                                standingPinsList.Add(false);
+                            }
+                        }
+                        // A cool snippet i found on the World Wide Web, for shuffling the contents of a list.
+                        standingPinsList = standingPinsList.OrderBy(x => Guid.NewGuid()).ToList();
+
+                        DrawPins(standingPinsList);
+                    }
+                    else if (i == 2) {
+                        secondThrow = rand.Next(0, 11 - firstThrow);
+                        DrawFrame(firstThrow, secondThrow);
+
+                        // Code might be a bit inefficient, but it stubbornly searches for pins to knock down based on secondThrow.
+                        int isThisKnocked;
+                        int knockedPins = 0;
+                        while (knockedPins < secondThrow) {
+                            isThisKnocked = rand.Next(0, 10);
+                            if (standingPinsList[isThisKnocked]) {
+                                standingPinsList[isThisKnocked] = false;
+                                knockedPins++;
+                            }
+                            if (! standingPinsList.Contains(true)) {
+                                break;
+                            }
+                        }
+
+                        DrawPins(standingPinsList);
+                    }
+
+                    Console.WriteLine();
+
+                    if (i + 1 < 3 && firstThrow != 10) {
+                        Console.WriteLine("Press Enter to throw.");
+                        while (Console.ReadKey().Key != ConsoleKey.Enter) {
+                        }
+                        Console.Clear();
+                    }
+                    else {
+                        Console.WriteLine("Press Enter to continue.");
+                        while (Console.ReadKey().Key != ConsoleKey.Enter) {
+                        }
+                        Console.Clear();
+                    }
+
+                    if (firstThrow == 10) {
+                        i = 3;
+                        firstThrow = 0;
+                    }
+                }
+            }
+        }
+
+        static void DrawFrame(int firstRoll = -1, int secondRoll = -1) {
+            string first = " ";
+            string second = " ";
+
+            Console.Write("+-----");
+            Console.Write("+");
+            Console.WriteLine();
+
+            if (firstRoll != 10) {
+                if (firstRoll == 0) {
+                    first = "-";
+                }
+                else if (firstRoll == -1) {
+                    first = " ";
+                }
+                else {
+                    first = firstRoll.ToString();
+                }
+
+                if (secondRoll == 0) {
+                    second = "-";
+                }
+                else if (secondRoll + firstRoll == 10) {
+                    second = "/";
+                }
+                else if (secondRoll == -1) {
+                    second = " ";
+                }
+                else {
+                    second = secondRoll.ToString();
+                }
+            }
+            else {
+                first = "X";
+                second = " ";
+            }
+
+            Console.Write("| ");
+            Console.Write($"|{first}");
+            Console.Write($"|{second}");
+            Console.Write("|");
+            Console.WriteLine();
+            Console.Write("+ ----");
+            Console.Write("+");
+            Console.WriteLine();
+            Console.Write("+     ");
+            Console.Write("+");
+            Console.WriteLine();
+            Console.Write("+-----");
+            Console.Write("+");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        static void DrawPins(List<bool> standingPinsList) {
             Console.WriteLine("Current pins:");
             Console.WriteLine();
 
             // back row
-            DrawRow(7, 10, pinsStanding);
+            DrawRow(7, 10, standingPinsList);
 
             // middleback row
-            DrawRow(4, 6, pinsStanding, " ");
+            DrawRow(4, 6, standingPinsList, " ");
 
             // middlefront row
-            DrawRow(2, 3, pinsStanding, "  ");
+            DrawRow(2, 3, standingPinsList, "  ");
 
             // front pin
-            DrawRow(1, 1, pinsStanding, "   ");
-
-            Console.WriteLine();
-            Console.WriteLine("--");
-            Console.WriteLine();
+            DrawRow(1, 1, standingPinsList, "   ");
         }
 
-        static void DrawRow(int startpin, int lastpin, List<bool> pinList, string padding = "") {
+        static void DrawRow(int startpin, int lastpin, List<bool> standingPinsList, string padding = "") {
             Console.Write(padding);
             for (int i = startpin - 1; i < lastpin; i++) {
-                if (pinList[i]) {
+                if (standingPinsList[i]) {
                     Console.Write("o ");
                 }
                 else {
