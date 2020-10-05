d75cc06466332bfbf3413525bd219c391e4be569
yohapii <37801852+yohapii@users.noreply.github.com>
2020-10-05 16:53:10 +0200

    Made the tank move towards player and removed shell limit, wtih gameover on tank reaching player

--- w02d04m02/w02d04m02/Program.cs
+++ w02d04m02/w02d04m02/Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Media;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace w02d04m02 {
     class Program {
@@ -17,7 +12,6 @@ namespace w02d04m02 {
             string tankGraphic = "T";
             string explosionGraphic = "B   (oom)";
             int playingField = 80;
-            int shellAmount = 5;
             int distanceToTank = dice.Next(40, 71);
             bool gameIsOn = true;
             bool gameIsWin = false;
@@ -29,7 +23,66 @@ namespace w02d04m02 {
 
             string name = Console.ReadLine();
 
+            while (gameIsOn && distanceToTank > 0) {
+                Console.Clear();
+                DrawBattlefield(artilleryGraphic, tankGraphic, distanceToTank, playingField);
+
+                Console.WriteLine();
+                Console.WriteLine($"Aim your shot, {name}!");
+                Console.Write("Enter distance: ");
+
+                string numberText = Console.ReadLine();
+                int aimedDistance = Int32.Parse(numberText) - 1;
+
+                for (int i = 0; i < artilleryGraphic.Length + aimedDistance; i++) {
+                    Console.Write(" ");
+                }
+
+                Console.WriteLine(explosionGraphic);
+
+                if (aimedDistance == distanceToTank) {
+                    Console.WriteLine($"You hit the tank! Good job, {name}!");
+
+                    gameIsOn = false;
+                    gameIsWin = true;
+                }
+                else if (aimedDistance > distanceToTank) {
+                    Console.WriteLine("No... You aimed too far.");
+
+                    distanceToTank -= dice.Next(1, 16);
+
+                    Console.WriteLine();
+                    Console.WriteLine("Press Enter to see the new battlefield.");
+                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                }
+                else {
+                    Console.WriteLine("Oops, that aim was too short.");
+
+                    distanceToTank -= dice.Next(1, 16);
+
+                    Console.WriteLine();
+                    Console.WriteLine("Press Enter to see the new battlefield.");
+                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                }
+            }
+
+            if (gameIsWin) {
+                Console.WriteLine();
+                Console.WriteLine("YOU WIN!");
+            }
+            else {
+                Console.Clear();
+                DrawBattlefield(artilleryGraphic, tankGraphic, distanceToTank, playingField);
+
+                Console.WriteLine();
+                Console.WriteLine("The tank is here now, that means:");
+                Console.WriteLine("GAME OVER, NOOB.");
+            }
+
             Console.WriteLine();
+        }
+
+        static void DrawBattlefield(string artilleryGraphic, string tankGraphic, int distanceToTank, int playingField) {
             Console.WriteLine($"Here is the map of the battlefield:");
             Console.WriteLine();
             Console.Write($"{artilleryGraphic}");
@@ -42,49 +95,6 @@ namespace w02d04m02 {
 
             for (int i = 0; i < playingField - distanceToTank - artilleryGraphic.Length - 1; i++) {
                 Console.Write("_");
-            }
-
-            Console.WriteLine();
-
-            while (gameIsOn && shellAmount > 0) {
-                //Console.Write("Distance to tank: " + distanceToTank);
-
-                Console.WriteLine();
-                Console.WriteLine($"Aim your shot, {name}!");
-                Console.Write("Enter distance: ");
-
-                string numberText = Console.ReadLine();
-                int aimedDistance = Int32.Parse(numberText);
-
-                for (int i = 0; i < artilleryGraphic.Length + aimedDistance; i++) {
-                    Console.Write(" ");
-                }
-
-                Console.WriteLine(explosionGraphic);
-
-                //Console.Write("You entered: " + aimedDistance);
-
-                if (aimedDistance == distanceToTank) {
-                    Console.WriteLine($"You hit the tank! Good job, {name}!");
-                    gameIsOn = false;
-                    gameIsWin = true;
-                }
-                else if (aimedDistance > distanceToTank) {
-                    Console.WriteLine("No... You aimed too far.");
-                    shellAmount--;
-                }
-                else {
-                    Console.WriteLine("Oops, that aim was too short.");
-                    shellAmount--;
-                }
-            }
-            if (gameIsWin) {
-                Console.WriteLine();
-                Console.WriteLine("YOU WIN!");
-            }
-            else {
-                Console.WriteLine();
-                Console.WriteLine("GAME OVER, NOOB.");
             }
 
             Console.WriteLine();
