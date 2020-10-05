3b0d226ef979b2299629f3c227e95dc8df74c491
yohapii <37801852+yohapii@users.noreply.github.com>
2020-10-05 16:55:09 +0200

    Week 4 Day 5 Mission 1

---
+++ w04d05m01/w04d05m01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Chart {
+    class Program {
+        static Random rand = new Random();
+
+        static void Main(string[] args) {
+            var data = new List<double>();
+
+            for (int i = 0; i < 70; i++) {
+                data.Add(rand.Next(20));
+                DisplayData(data);
+            }
+        }
+
+        static void DisplayData(List<double> data) {
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(0, 0);
+
+            for (int y = 20; y >= 0; y--) {
+                if (y % 5 == 0) {
+                    Console.Write($"{y,3} |");
+                }
+                else {
+                    Console.Write("    |");
+                }
+
+                for (int x = 0; x < data.Count; x++) {
+                    if (y == 0) {
+                        Console.Write("-");
+                        continue;
+                    }
+
+                    Console.Write(y <= data[x] ? "\u2592" : " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Thread.Sleep(10);
+        }
+    }
+}
---
+++ w04d05m01/w04d05m01/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("w04d05m01")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("w04d05m01")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c7bd2ab5-666a-4ebc-a96b-1a6ebf720bfb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
