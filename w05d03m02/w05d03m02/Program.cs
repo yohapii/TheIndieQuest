e5619b33642ea1787095c1851f7cf4583363a23c
yohapii <37801852+yohapii@users.noreply.github.com>
2020-10-07 15:59:09 +0200

    Week 5 Day 3 Mission 2

---
+++ w05d03m02/w05d03m02/Program.cs
@@ -0,0 +1,27 @@
+﻿/*  Outputs an ASCII chart.
+ */
+
+using System;
+
+namespace w05d03m02 {
+    class Program {
+        static void Main(string[] args) {
+            for (int i = 0; i <= 255; i++) {
+                if (i < 10) {
+                    Console.Write("  ");
+                }
+                else if (i < 100) {
+                    Console.Write(" ");
+                }
+                if (i == 10) {
+                    Console.WriteLine($"{i} = (line break)");
+                }
+                else {
+                    Console.WriteLine($"{i} = {(char)i}");
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
