27776817d76371311999c0189e43b66119f85652
yohapii <37801852+yohapii@users.noreply.github.com>
2020-10-11 16:13:38 +0200

    Week 3 Day 4 Mission 2

---
+++ w03d04m02/w03d04m02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace w03d04m02 {
+    class Program {
+        static void Main(string[] args) {
+            FactorialList(10);
+        }
+
+        static void FactorialList(int amount = 0) {
+            for (int i = 0; i <= amount; i++) {
+                Console.WriteLine($"{i}! = {Factorial(i)}");
+            }
+        }
+
+        static int Factorial(int n = 0) {
+            if (n == 0) {
+                return 1;
+            }
+            else {
+                return n * Factorial(n - 1);
+            }
+        }
+    }
+}
