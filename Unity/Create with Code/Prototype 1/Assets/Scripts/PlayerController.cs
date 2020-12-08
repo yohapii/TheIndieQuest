03e2e6aa92f779d3b15b090d86dacdbfaedd59cc
yohapii <37801852+yohapii@users.noreply.github.com>
2020-12-08 12:23:47 +0100

    Finished lesson 1.2

---
+++ Unity/Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+    }
+}
