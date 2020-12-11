9ad93947cfec1bee0aaed56407814c93103d5421
yohapii <37801852+yohapii@users.noreply.github.com>
2020-12-11 17:54:50 +0100

    Finished Challenge 1

---
+++ Unity/Create with Code/Prototype 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour {
+    public GameObject plane;
+    private Vector3 offset = new Vector3(20, 3, 0);
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.position = plane.transform.position + offset;
+    }
+}
