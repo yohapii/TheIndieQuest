152849d12298693277136bb2f0ee6d2fa08fb2ab
yohapii <37801852+yohapii@users.noreply.github.com>
2020-12-15 18:01:34 +0100

    Finished Lesson 2.4

--- Unity/Create with Code/Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Unity/Create with Code/Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -2,7 +2,7 @@
 
 public class DestroyOutOfBounds : MonoBehaviour {
     private float topBound = 30.0f;
-    private float lowerBound = -10.0f;
+    private float lowerBound = -4.0f;
 
     // Start is called before the first frame update
     void Start() {
@@ -11,10 +11,12 @@ public class DestroyOutOfBounds : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        // If an object goes past the player's view in the game, remove that object
         if (transform.position.z > topBound) {
             Destroy(gameObject);
         }
         else if (transform.position.z < lowerBound) {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
     }
