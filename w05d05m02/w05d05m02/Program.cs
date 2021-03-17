e14246674f82040df092905193815e866b879e57
yohapii <37801852+yohapii@users.noreply.github.com>
2021-03-17 19:46:17 +0100

    Week 5 day 5 mission 2 complete

---
+++ w05d05m02/w05d05m02/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace w05d05m02 {
+    class Program {
+        static void Main(string[] args) {
+            string fileName = "message.txt";
+            string path = Path.Combine(Environment.CurrentDirectory, fileName);
+            string readText = File.ReadAllText(path);
+
+            string[] readWords = readText.Split(',', '.', ' ');
+
+            List<string> phoneNumbers = new List<string>();
+
+            for (int i = 0; i < readWords.Length; i++) {
+                if (IsPhoneNumber(readWords[i])) {
+                    phoneNumbers.Add(readWords[i]);
+                }
+            }
+
+            Console.WriteLine("The phone numbers present in the file are:");
+
+            for (int i = 0; i < phoneNumbers.Count; i++) {
+                Console.WriteLine(phoneNumbers[i]);
+            }
+
+            Console.WriteLine();
+        }
+
+        static bool IsPhoneNumber(string input) {
+            if (input.Length == 12) {
+                string[] splitInput = input.Split('-');
+                if (splitInput.Length == 3) {
+                    if (splitInput[0].Length == 3 && splitInput[1].Length == 3 && splitInput[2].Length == 4) {
+                        for (int i = 0; i < splitInput.Length; i++) {
+                            for (int j = 0; j < splitInput[i].Length; j++) {
+                                if (!Char.IsDigit(splitInput[i][j])) {
+                                    return false;
+                                }
+                            }
+                        }
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
