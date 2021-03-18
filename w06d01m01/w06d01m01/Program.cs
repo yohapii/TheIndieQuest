448f22af6b7ee8a9f278a79357429bb05b7fe13d
yohapii <37801852+yohapii@users.noreply.github.com>
2021-03-18 16:28:00 +0100

    Week 6 day 1 mission 1 complete

---
+++ w06d01m01/w06d01m01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace w06d01m01 {
+    class Program {
+        static void Main(string[] args) {
+            string fileName = "MonsterManual.txt";
+            string path = Path.Combine(Environment.CurrentDirectory, fileName);
+            string[] readTextLines = File.ReadAllLines(path);
+
+            List<string> monsterNames = new List<string>();
+
+            for (int i = 0; i < readTextLines.Length; i++) {
+                if (i == 0 || readTextLines[i - 1] == "") {
+                    monsterNames.Add(readTextLines[i]);
+                }
+            }
+
+            Console.WriteLine("Monsters in the manual are:");
+            Console.WriteLine($"{string.Join("\n", monsterNames)}");
+            Console.WriteLine();
+        }
+    }
+}
