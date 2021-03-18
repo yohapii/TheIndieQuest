9b0ceaa32dd07bb71c5099636085aaff271ac91f
yohapii <37801852+yohapii@users.noreply.github.com>
2021-03-18 16:56:46 +0100

    Week 6 day 1 mission 2 complete

---
+++ w06d01m02/w06d01m02/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace w06d01m02 {
+    class Program {
+        static void Main(string[] args) {
+            string fileName = "MonsterManual.txt";
+            string path = Path.Combine(Environment.CurrentDirectory, fileName);
+            string[] readTextLines = File.ReadAllLines(path);
+
+            List<string> monsterNames = new List<string>();
+            List<bool> monsterCanFly = new List<bool>();
+
+            for (int i = 0; i < readTextLines.Length; i++) {
+                if (i == 0 || readTextLines[i - 1] == "") {
+                    monsterNames.Add(readTextLines[i]);
+                }
+
+                string sPattern = "Speed:";
+                if (Regex.IsMatch(readTextLines[i], sPattern)) {
+                    sPattern = "fly";
+                    if (Regex.IsMatch(readTextLines[i], sPattern)) {
+                        monsterCanFly.Add(true);
+                    }
+                    else {
+                        monsterCanFly.Add(false);
+                    }
+                }
+            }
+
+            Console.WriteLine("Monsters in the manual are:");
+            for (int i = 0; i < monsterNames.Count; i++) {
+                Console.WriteLine($"{monsterNames[i]} - Can fly: {monsterCanFly[i]}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
